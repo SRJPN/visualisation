dfb30658b7b344a4fcb4ef08a47f22d092394df2
Sooraj Parameswaran <soorajp@thoughtworks.com>
2016-11-11 12:02:20 +0530

    Refactoring assignment 4

--- javascript/assignment4.js
+++ javascript/assignment4.js
@@ -1,10 +1,9 @@
-const LIMIT = 10;
+const LIMIT = 10,
+    TABLE_CLASS = 'table-bordered',
+    CELL_WIDTH = '130px',
+    HEADING = 'Title';
 
-var generateValues = function(limit) {
-    return [...new Array(limit)].map((d, i) => i + 1);
-};
-
-var rows = [{
+const ROW_TITLES = [{
     name: 'n',
     formula: d3.scaleLinear()
 }, {
@@ -12,56 +11,68 @@ var rows = [{
     formula: d3.scalePow().exponent(2)
 }, {
     name: 'log(n)',
-    formula: d3.scaleLog()
+    formula: (d) => logScale(d).toFixed(2)
 }, {
     name: 'log(n) rounded',
     formula: d3.scaleLog().rangeRound([0, 1])
-}]
+}];
 
+var table, logScale = d3.scaleLog();
 
-var tabulate = function(heading, rows_values, values) {
-    var table = d3.select('.container').append('table')
-    var thead = table.append('thead')
-    var tbody = table.append('tbody');
+var generateValues = function(limit) {
+    return [...new Array(limit)].map((d, i) => i + 1);
+};
 
-    table.attr('class', 'table-bordered')
-        .style('text-align', 'center');
+var self = (thing) => thing;
+
+var valuesMapper = function(values) {
+    return (action) => [action.name].concat(values.map((value) => action['formula'](value)));
+};
+
+var generateTableHead = function(heading, values) {
+    var heading_row = [heading].concat(values);
+
+    var thead = table.append('thead');
 
     thead.append('tr')
         .selectAll('th')
-        .data(heading).enter()
+        .data(heading_row)
+        .enter()
         .append('th')
-        .style('width', '130px')
+        .style('width', CELL_WIDTH)
         .style('text-align', 'center')
-        .text(function(heading) {
-            return heading;
-        });
+        .text(self);
+
+    return thead;
+};
+
+var generateTableBody = function(titles, values) {
+    var tbody = table.append('tbody');
 
     var rows = tbody.selectAll('tr')
-        .data(rows_values)
+        .data(titles)
         .enter()
         .append('tr');
 
     var cells = rows.selectAll('td')
-        .data(function(row) {
-            var rowInput = values.map(function(value) {
-                return row['formula'](value).toFixed(2)
-            });
-            rowInput.unshift(row.name);
-            return rowInput
-        })
+        .data(valuesMapper(values))
         .enter()
         .append('td')
-        .text(function(d) {
-            return d;
-        });
+        .text(self);
+
+    return cells;
+};
+
+var genrateTable = function() {
+    table = d3.select('.container').append('table')
+
+    table.attr('class', TABLE_CLASS)
+        .style('text-align', 'center');
 };
 
 window.onload = function() {
-    var heading = generateValues(LIMIT)
-    heading.unshift('Title');
-
-    values = generateValues(LIMIT)
-
-    tabulate(heading, rows, values);
-}
+    var values = generateValues(LIMIT)
+    genrateTable();
+    generateTableHead(HEADING, values);
+    generateTableBody(ROW_TITLES, values);
+};
