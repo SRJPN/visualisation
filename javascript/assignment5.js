ababcab3e428e9844f9eb22ccdedd5d8d12c5e62
Sooraj Parameswaran <soorajp@thoughtworks.com>
2016-11-11 14:07:26 +0530

    Refactoring Assignment 5

--- javascript/assignment5.js
+++ javascript/assignment5.js
@@ -3,15 +3,15 @@ const WIDTH_RANGE = [12, 120];
 const HEIGHT_RANGE = [30, 180];
 
 var widthScale = d3.scaleLinear()
-    .domain([1, LIMIT])
+    .domain([0, LIMIT])
     .range(WIDTH_RANGE);
 
 var heightScale = d3.scaleLinear()
-    .domain([1, LIMIT])
+    .domain([0, LIMIT])
     .range(HEIGHT_RANGE)
 
 var generateValues = function(limit) {
-    return [...new Array(limit)].map((d, i) => i + 1);
+    return [...new Array(limit + 1)].map((d, i) => i);
 };
 
 var generateBoxes = function(values) {
@@ -22,9 +22,13 @@ var generateBoxes = function(values) {
     boxes.enter()
         .append('div')
         .classed('box', true)
-        .style('font', (d) => 'italic bold ' + widthScale(d) + 'px/' + heightScale(d) + 'px sans-serif')
+        .style('font', generateFontString)
         .text((d) => d)
-}
+};
+
+var generateFontString = function(number) {
+    return ['italic bold ', widthScale(number), 'px/', heightScale(number), 'px Georgia sans-serif'].join('');
+};
 
 window.onload = function() {
     var values = generateValues(LIMIT)
