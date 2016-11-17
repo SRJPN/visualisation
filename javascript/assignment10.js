8645c5b969ecdadfa7222a4dbbff5d117fe06813
Sooraj Parameswaran <soorajp@thoughtworks.com>
2016-11-17 16:43:28 +0530

    Completed Assignment 10

---
+++ javascript/assignment10.js
@@ -0,0 +1,31 @@
+const sinLinePoints = [...new Array(10)].map((d, i) => [i, i]);
+
+const INCREMENTER = 5,
+    RADIUS_OF_CIRCLE = 3.5;
+
+var decoratedMiscScale = function([x, y]) {
+    var scale = d3.scaleLinear()
+        .domain([0, 1])
+        .range([INNER_HEIGHT, 0]);
+    return scale(swamijiFormula(x));
+};
+
+var swamijiFormula = function(number) {
+    return (Math.sin(3 * number) + 1) / 2
+};
+
+var lineDetails = {
+    'xRefiner': decoratedXScale,
+    'yRefiner': decoratedMiscScale,
+    'curve': d3.curveLinear,
+    'color': 'steelblue'
+};
+
+window.onload = function() {
+    var container = d3.select('.container');
+
+    var chartArea = createGraph(container);
+
+    drawAndDecorateLine(chartArea, sinLinePoints, lineDetails);
+
+};
