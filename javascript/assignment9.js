f5094caf58e57ea19a8621c75e4990780d0ef6ce
Sooraj Parameswaran <soorajp@thoughtworks.com>
2016-11-17 16:30:44 +0530

    Completed Assignment 9

---
+++ javascript/assignment9.js
@@ -0,0 +1,71 @@
+const points = [
+    [0, 5],
+    [1, 9],
+    [2, 7],
+    [3, 5],
+    [4, 3],
+    [6, 4],
+    [7, 2],
+    [8, 3],
+    [9, 2]
+];
+
+const sinLinePoints = [...new Array(10)].map((d, i) => [i, i]);
+
+const INCREMENTER = 5,
+    RADIUS_OF_CIRCLE = 3.5;
+
+var curveArray = [{
+    "d3Curve": d3.curveLinear,
+    "curveTitle": "curveLinear"
+}, {
+    "d3Curve": d3.curveLinearClosed,
+    "curveTitle": "curveLinear"
+}, {
+    "d3Curve": d3.curveStep,
+    "curveTitle": "curveStep"
+}, {
+    "d3Curve": d3.curveStepBefore,
+    "curveTitle": "curveStepBefore"
+}, {
+    "d3Curve": d3.curveStepAfter,
+    "curveTitle": "curveStepAfter"
+}, {
+    "d3Curve": d3.curveBasis,
+    "curveTitle": "curveBasis"
+}, {
+    "d3Curve": d3.curveCardinal,
+    "curveTitle": "curveCardinal"
+}, {
+    "d3Curve": d3.curveMonotoneX,
+    "curveTitle": "curveMonotoneX"
+}, {
+    "d3Curve": d3.curveCatmullRom,
+    "curveTitle": "curveCatmullRom"
+}];
+
+window.onload = function() {
+    var container = d3.select('.container');
+
+    curveArray.forEach(function(curve) {
+
+        var straightLineDetails = {
+            'xRefiner': decoratedXScale,
+            'yRefiner': decoratedYScale,
+            'curve': curve.d3Curve,
+            'color': 'steelblue'
+        };
+
+        var sineLineDetails = {
+            'xRefiner': decoratedXScale,
+            'yRefiner': decoratedSineScale,
+            'curve': curve.d3Curve,
+            'color': 'brown'
+        };
+
+        var chartArea = createGraph(container);
+
+        drawAndDecorateLine(chartArea, points, straightLineDetails);
+        drawAndDecorateLine(chartArea, sinLinePoints, sineLineDetails);
+    })
+};
