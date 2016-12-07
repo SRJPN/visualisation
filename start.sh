1b1d1e28eae536dced10d3b10a9c61c32d70619c
Sooraj Parameswaran <soorajp@thoughtworks.com>
2016-12-07 17:57:36 +0530

    shell to generate json file

--- start.sh
+++ start.sh
@@ -1,1 +1,32 @@
+skip="index"
+declare -a files
+fileName="files.json"
+
+for entry in ./*.html
+do
+  tmp=${entry#*/}
+  tmp=${tmp%.*}
+  if [ "$tmp" == "$skip" ]
+  then
+    echo $tmp
+  else
+    files+=($tmp)
+  fi
+done
+
+pos=$(( ${#files[*]} - 1 ))
+last=${files[$pos]}
+
+echo "[" > files.json
+for FILE in "${files[@]}"
+do
+  if [[ $FILE == $last ]]
+  then
+    echo "\"$FILE\"" >> $fileName
+  else
+    echo "\"$FILE\"," >> $fileName
+  fi
+done
+echo "]" >> files.json
+
 python -m SimpleHTTPServer 3000
